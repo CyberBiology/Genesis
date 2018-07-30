463d1097bae5060252039427c25f762ae142019b
CyberBi <CyberBi@users.noreply.github.com>
2018-07-30 10:21:12 +0300

    1 pexel's bots

--- src/World.java
+++ src/World.java
@@ -5,20 +5,26 @@ import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+//import java.util.concurrent.locks.Lock;
+//import java.util.concurrent.locks.ReentrantLock;
+
 
 // Основной класс программы.
 public class World extends JFrame {
 
     public int width;
     public int height;
+    public int zoom;
     public int sealevel;
     public int drawstep;
     public int[][] map;    //Карта мира
-    public Color[][] mapview;    //Карта мира
+    public int[] mapInGPU;    //Карта для GPU
     public Image mapbuffer = null;
     public Bot[][] matrix;    //Матрица мира
+    public Bot zerobot = new Bot();
+    public Bot currentbot;
     public int generation;
     public int population;
     public int organic;
@@ -47,19 +53,20 @@ public class World extends JFrame {
 
 
     JRadioButton baseButton = new JRadioButton("Base", true);
+    JRadioButton combinedButton = new JRadioButton("Combined", false);
     JRadioButton energyButton = new JRadioButton("Energy", false);
     JRadioButton mineralButton = new JRadioButton("Minerals", false);
-    JRadioButton combinedButton = new JRadioButton("Combined", false);
     JRadioButton ageButton = new JRadioButton("Age", false);
 
     public World() {
     	
         simulation = this;
 
+        zoom = 1;
         sealevel = 145;
         drawstep = 10;
 
-        setTitle("Genesis 1.1.0");
+        setTitle("Genesis 1.2.0");
         setSize(new Dimension(1800, 900));
         Dimension sSize = Toolkit.getDefaultToolkit().getScreenSize(), fSize = getSize();
         if (fSize.height > sSize.height) fSize.height = sSize.height;
@@ -143,14 +150,14 @@ public class World extends JFrame {
 
         ButtonGroup group = new ButtonGroup();
         group.add(baseButton);
+        group.add(combinedButton);
         group.add(energyButton);
         group.add(mineralButton);
-        group.add(combinedButton);
         group.add(ageButton);
         toolbar.add(baseButton);
+        toolbar.add(combinedButton);
         toolbar.add(energyButton);
         toolbar.add(mineralButton);
-        toolbar.add(combinedButton);
         toolbar.add(ageButton);
 
         this.pack();
@@ -178,8 +185,8 @@ public class World extends JFrame {
 
     class mapButtonAction implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            width = canvas.getWidth() / 2;    // Ширина доступной части экрана для рисования карты
-            height = canvas.getHeight() / 2;
+            width = canvas.getWidth() / zoom;    // Ширина доступной части экрана для рисования карты
+            height = canvas.getHeight() / zoom;
             generateMap((int) (Math.random() * 10000));
             generateAdam();
             paintMapView();
@@ -206,95 +213,100 @@ public class World extends JFrame {
         int mapred;
         int mapgreen;
         int mapblue;
-        mapbuffer = canvas.createImage(width * 2, height * 2); // ширина - высота картинки
+        mapbuffer = canvas.createImage(width * zoom, height * zoom); // ширина - высота картинки
         Graphics g = mapbuffer.getGraphics();
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                if (map[x][y] < sealevel) {                     // подводная часть
-                    mapred = 5;
-                    mapblue = 140 - (sealevel - map[x][y]) * 3;
-                    mapgreen = 150 - (sealevel - map[x][y]) * 10;
-                    if (mapblue < 20) mapblue = 20;
-                    if (mapgreen < 10) mapgreen = 10;
-                } else {                                        // надводная часть
-                    mapred = (int)(150 + (map[x][y] - sealevel) * 2.5);
-                    mapgreen = (int)(100 + (map[x][y] - sealevel) * 2.6);
-                    mapblue = 50 + (map[x][y] - sealevel) * 3;
-                    if (mapred > 255) mapred = 255;
-                    if (mapblue > 255) mapblue = 255;
-                    if (mapgreen > 255) mapgreen = 255;
-                }
-                g.setColor(new Color(mapred, mapgreen, mapblue));
-                g.fillRect(x * 2, y * 2, 2, 2);
+
+        final BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        final int[] rgb = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+
+        for (int i = 0; i < rgb.length; i++) {
+            if (mapInGPU[i] < sealevel) {                     // подводная часть
+                mapred = 5;
+                mapblue = 140 - (sealevel - mapInGPU[i]) * 3;
+                mapgreen = 150 - (sealevel - mapInGPU[i]) * 10;
+                if (mapgreen < 10) mapgreen = 10;
+                if (mapblue < 20) mapblue = 20;
+            } else {                                        // надводная часть
+                mapred = (int)(150 + (mapInGPU[i] - sealevel) * 2.5);
+                mapgreen = (int)(100 + (mapInGPU[i] - sealevel) * 2.6);
+                mapblue = 50 + (mapInGPU[i] - sealevel) * 3;
+                if (mapred > 255) mapred = 255;
+                if (mapgreen > 255) mapgreen = 255;
+                if (mapblue > 255) mapblue = 255;
             }
+            rgb[i] = (mapred << 16) | (mapgreen << 8) | mapblue;
         }
+        g.drawImage(image, 0, 0, null);
+
+
     }
 
 
 //    @Override
     public void paint1() {
-    	Image buf = canvas.createImage(width * 2, height * 2);; //Создаем временный буфер для рисования
-    	Graphics g = buf.getGraphics(); //подеменяем графику на временный буфер
+
+        Image buf = canvas.createImage(width * zoom, height * zoom); //Создаем временный буфер для рисования
+        Graphics g = buf.getGraphics(); //подеменяем графику на временный буфер
         g.drawImage(mapbuffer, 0, 0, null);
+
+        final BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        final int[] rgb = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
 
         population = 0;
         organic = 0;
-        int mapred;
-        int mapgreen;
-        int mapblue;
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                if (matrix[x][y] != null) {
-                    if (matrix[x][y].alive == 3) {                      // живой бот
-                        if (baseButton.isSelected()) {
-                            g.setColor(new Color(matrix[x][y].c_red, matrix[x][y].c_green, matrix[x][y].c_blue));
-                        } else if (energyButton.isSelected()) {
-                            mapgreen = 255 - (int) (matrix[x][y].health * 0.25);
-                            if (mapgreen < 0) mapgreen = 0;
-                            g.setColor(new Color(255, mapgreen, 0));
-                        } else if (mineralButton.isSelected()) {
-                            mapblue = 255 - (int) (matrix[x][y].mineral * 0.5);
-                            if (mapblue < 0) mapblue = 0;
-                            g.setColor(new Color(0, 255, mapblue));
-                        } else if (combinedButton.isSelected()) {
-                            mapgreen = (int) (matrix[x][y].c_green * (1 - matrix[x][y].health * 0.0005));
-                            if (mapgreen < 0) mapgreen = 0;
-                            mapblue = (int) (matrix[x][y].c_blue * (0.8 - matrix[x][y].mineral * 0.0005));
-                            g.setColor(new Color(matrix[x][y].c_red, mapgreen, mapblue));
-                        } else if (ageButton.isSelected()) {
-                            mapred = 255 - (int) (Math.sqrt(matrix[x][y].age) * 4);
-                            if (mapred < 0) mapred = 0;
-                            g.setColor(new Color(mapred, 0, 255));
-                        }
-                        g.fillRect(x * 2, y * 2, 2, 2);
-                        population++;
-                    } else if (matrix[x][y].alive == 1) {                                            // органика, известняк, коралловые рифы
-                        if (map[x][y] < sealevel) {                     // подводная часть
-                            mapred = 20;
-                            mapblue = 160 - (sealevel - map[x][y]) * 2;
-                            mapgreen = 170 - (sealevel - map[x][y]) * 4;
-                            if (mapblue < 40) mapblue = 40;
-                            if (mapgreen < 20) mapgreen = 20;
-                        } else {                                    // скелетики, трупики на суше
-                            mapred = (int) (80 + (map[x][y] - sealevel) * 2.5);   // надводная часть
-                            mapgreen = (int) (60 + (map[x][y] - sealevel) * 2.6);
-                            mapblue = 30 + (map[x][y] - sealevel) * 3;
-                            if (mapred > 255) mapred = 255;
-                            if (mapblue > 255) mapblue = 255;
-                            if (mapgreen > 255) mapgreen = 255;
-                        }
-                        g.setColor(new Color(mapred, mapgreen, mapblue));
-                        g.fillRect(x * 2, y * 2, 2, 2);
-                        organic++;
-                    }
+        int mapred, mapgreen, mapblue;
+
+        while (currentbot != zerobot) {
+            if (currentbot.alive == 3) {                      // живой бот
+                if (baseButton.isSelected()) {
+                    rgb[currentbot.y * width + currentbot.x] = (255 << 24) | (currentbot.c_red << 16) | (currentbot.c_green << 8) | currentbot.c_blue;
+                } else if (energyButton.isSelected()) {
+                    mapgreen = 255 - (int) (currentbot.health * 0.25);
+                    if (mapgreen < 0) mapgreen = 0;
+                    rgb[currentbot.y * width + currentbot.x] = (255 << 24) | (255 << 16) | (mapgreen << 8) | 0;
+                } else if (mineralButton.isSelected()) {
+                    mapblue = 255 - (int) (currentbot.mineral * 0.5);
+                    if (mapblue < 0) mapblue = 0;
+                    rgb[currentbot.y * width + currentbot.x] = (255 << 24) | (0 << 16) | (255 << 8) | mapblue;
+                } else if (combinedButton.isSelected()) {
+                    mapgreen = (int) (currentbot.c_green * (1 - currentbot.health * 0.0005));
+                    if (mapgreen < 0) mapgreen = 0;
+                    mapblue = (int) (currentbot.c_blue * (0.8 - currentbot.mineral * 0.0005));
+                    rgb[currentbot.y * width + currentbot.x] = (255 << 24) | (currentbot.c_red << 16) | (mapgreen << 8) | mapblue;
+                } else if (ageButton.isSelected()) {
+                    mapred = 255 - (int) (Math.sqrt(currentbot.age) * 4);
+                    if (mapred < 0) mapred = 0;
+                    rgb[currentbot.y * width + currentbot.x] = (255 << 24) | (mapred << 16) | (0 << 8) | 255;
                 }
+                population++;
+            } else if (currentbot.alive == 1) {                                            // органика, известняк, коралловые рифы
+                if (map[currentbot.x][currentbot.y] < sealevel) {                     // подводная часть
+                    mapred = 20;
+                    mapblue = 160 - (sealevel - map[currentbot.x][currentbot.y]) * 2;
+                    mapgreen = 170 - (sealevel - map[currentbot.x][currentbot.y]) * 4;
+                    if (mapblue < 40) mapblue = 40;
+                    if (mapgreen < 20) mapgreen = 20;
+                } else {                                    // скелетики, трупики на суше
+                    mapred = (int) (80 + (map[currentbot.x][currentbot.y] - sealevel) * 2.5);   // надводная часть
+                    mapgreen = (int) (60 + (map[currentbot.x][currentbot.y] - sealevel) * 2.6);
+                    mapblue = 30 + (map[currentbot.x][currentbot.y] - sealevel) * 3;
+                    if (mapred > 255) mapred = 255;
+                    if (mapblue > 255) mapblue = 255;
+                    if (mapgreen > 255) mapgreen = 255;
+                }
+                rgb[currentbot.y * width + currentbot.x] = (255 << 24) | (mapred << 16) | (mapgreen << 8) | mapblue;
+                organic++;
             }
+            currentbot = currentbot.next;
         }
+        currentbot = currentbot.next;
+
+        g.drawImage(image, 0, 0, null);
 
         generationLabel.setText(" Generation: " + String.valueOf(generation));
         populationLabel.setText(" Population: " + String.valueOf(population));
         organicLabel.setText(" Organic: " + String.valueOf(organic));
-        
+
         buffer = buf;
         canvas.repaint();
     }
@@ -304,15 +316,21 @@ public class World extends JFrame {
         public void run() {
             started	= true;         // Флаг работы потока, если false  поток заканчивает работу
             while (started) {       // обновляем матрицу
-                for (int y = 0; y < height; y++) {
-                    for (int x = 0; x < width; x++) {
-                        if (matrix[x][y] != null) {
-                            if (matrix[x][y].alive == 3) {
-                                if (matrix[x][y].lastgeneration < generation) matrix[x][y].step();        // выполняем шаг бота
-                            }
-                        }
-                    }
+//                for (int y = 0; y < height; y++) {
+//                    for (int x = 0; x < width; x++) {
+//                        if (matrix[x][y] != null) {
+//                            if (matrix[x][y].alive == 3) {
+//                                if (matrix[x][y].lastgeneration < generation) matrix[x][y].step();        // выполняем шаг бота
+//                            }
+//                        }
+//                    }
+//                }
+
+                while (currentbot != zerobot) {
+                    if (currentbot.alive == 3) currentbot.step();
+                    currentbot = currentbot.next;
                 }
+                currentbot = currentbot.next;
                 generation++;
                 if (generation % drawstep == 0) {             // отрисовка на экран через каждые ... шагов
                     paint1();                           // отображаем текущее состояние симуляции на экран
@@ -345,7 +363,6 @@ public class World extends JFrame {
     public void generateMap(int seed) {
         generation = 0;
         this.map = new int[width][height];
-        this.mapview = new Color[width][height];
         this.matrix = new Bot[width][height];
 
         Perlin2D perlin = new Perlin2D(seed);
@@ -356,11 +373,20 @@ public class World extends JFrame {
                 map[x][y] = (int)(value * 255 + 128) & 255;
             }
         }
+        mapInGPU = new int[width * height];
+        for (int i=0; i<width; i++) {
+            for(int j=0; j<height; j++) {
+                mapInGPU[j*width+i] = map[i][j];
+            }
+        }
     }
 
     // генерируем первого бота
     public void generateAdam() {
+
         Bot bot = new Bot();
+        zerobot.prev = bot;
+        zerobot.next = bot;
 
         bot.adr = 0;            // начальный адрес генома
         bot.x = width / 2;      // координаты бота
@@ -375,11 +401,14 @@ public class World extends JFrame {
         bot.direction = 5;      // направление
         bot.mprev = null;       // бот не входит в многоклеточные цепочки, поэтому ссылки
         bot.mnext = null;       // на предыдущего, следующего в многоклеточной цепочке пусты
-        for (int i = 0; i < 64; i++) {          // заполняем геном командой 25 - фотосинтез
-            bot.mind[i] = 25;
+        bot.prev = zerobot;     // ссылка на предыдущего
+        bot.next = zerobot;     // ссылка на следующего
+        for (int i = 0; i < 64; i++) {          // заполняем геном командой 32 - фотосинтез
+            bot.mind[i] = 32;
         }
 
         matrix[bot.x][bot.y] = bot;             // помещаем бота в матрицу
+        currentbot = bot;                       // устанавливаем текущим
     }
 
 
